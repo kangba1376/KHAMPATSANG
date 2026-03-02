8539869d7e14124cd9bace74377f4bc7371d2ddb
kangba1376 <kangba1376@gmail.com>
2026-03-02 13:58:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,19 @@
-const CACHE_NAME = 'soul-day-2026-03-02-15:05'; // 每次发布新消息，必须改这个时间
-const ASSETS = [
-  './',
-  './index.html',
-  './BZDHT.ttf',
-  './TibetWildYak.ttf',
-  './converter.js',
-  './rules_data.js',
-  './logo.png',
-  './favicon.ico',
-  './apple-touch-icon.png',
-  './manifest.json'
-];
+const CACHE_NAME = 'soul-day-2026-03-02-15:05';
 
-// 安装：立即接管
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
 });
 
-// 激活：暴力删除所有旧缓存
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keys.map((key) => caches.delete(key)));
     }).then(() => self.clients.claim())
   );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
 
 // 策略修改：网络优先。解决手机桌面版不刷新的核心
