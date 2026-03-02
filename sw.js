bb9af4adbeb6461241d114236f285b1e20255bde
kangba1376 <kangba1376@gmail.com>
2026-03-02 18:06:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'soul-day-2026-03-02-17:30'; // 自动更新为当前北京时间
+const CACHE_NAME = 'soul-day-2026-03-02-18:05'; // 自动更新为当前北京时间
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
